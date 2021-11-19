84b8794ac66cfcc614ad217c16fa10358fa80e35
Arthur Garcia Torrezani <79718431+tutugt@users.noreply.github.com>
2021-11-19 19:24:09 -0300

    Add files via upload

---
+++ provaart.sql
@@ -0,0 +1,47 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Nov 19, 2021 at 10:19 PM
+-- Server version: 5.7.31
+-- PHP Version: 7.3.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `provaart`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rihappy`
+--
+
+DROP TABLE IF EXISTS `rihappy`;
+CREATE TABLE IF NOT EXISTS `rihappy` (
+  `id` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `produto` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `preco` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `rihappy`
+--
+
+INSERT INTO `rihappy` (`id`, `produto`, `preco`) VALUES
+('4', '4', '7');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
